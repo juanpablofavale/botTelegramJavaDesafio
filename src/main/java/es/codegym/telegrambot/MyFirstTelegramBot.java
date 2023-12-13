629d2c095ff293476b2abc025426f22307519e00
jpf <juanpablofavale@hotmail.com>
2023-12-13 17:44:26 -0300

    estructura del juego del gatito hacker

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -6,6 +6,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.Map;
+
+import static es.codegym.telegrambot.TelegramBotContent.*;
+
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     public static String NAME;
@@ -22,8 +26,71 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         // ** para negrita
 
         if (getMessageText().equals("/start")){
-            sendTextMessageAsync("Excelente _futuro_ *desarrollador*! Comenzare a hackearte justo ahora!");
+            //sendTextMessageAsync("Excelente _futuro_ *desarrollador*! Comenzare a hackearte justo ahora!");
+            setUserGlory(0);
+            sendTextMessageAsync(STEP_1_TEXT, Map.of(
+                    "¡Hackear la Heladera!","step-1"
+            ));
         }
+
+        if (getCallbackQueryButtonKey().equals("step-1")){
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_2_TEXT, Map.of(
+                    "¡Tomar una salchicha! +20 de fama","step-2",
+                    "¡Tomar un pescado! +20 de fama","step-2",
+                    "¡Tirar una lata de pepinillos! +20 de fama","step-2"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-2")){
+            addUserGlory(20);
+            sendTextMessageAsync(STEP_3_TEXT, Map.of(
+                    "¡Hackear la aspiradora!","step-3"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-3")){
+            sendTextMessageAsync(STEP_4_TEXT, Map.of(
+                    "¡Enviar al robot aspirador a por comida! +30 de fama","step-4",
+                    "¡Dar un paseo en el robot aspirador! +30 de fama","step-4",
+                    "¡Huir del robot aspirador! +30 de fama","step-4"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-4")){
+            addUserGlory(30);
+            sendTextMessageAsync(STEP_5_TEXT, Map.of(
+                    "¡Encender y ponerse la GoPro!","step-5"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-5")){
+            addUserGlory(40);
+            sendTextMessageAsync(STEP_6_TEXT, Map.of(
+                    "¡Correr por los tejados, grabar con la GoPro! +40 de fama","step-6",
+                    "¡Atacar a otros gatos desde tu escondite con la GoPro! +40 de fama","step-6",
+                    "¡Atacar a los perros desde tu escondite con la GoPro! +40 de fama","step-6"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-6")){
+            addUserGlory(40);
+            sendTextMessageAsync(STEP_7_TEXT, Map.of(
+                    "¡Hackear la contraseña!","step-7"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-7")){
+            addUserGlory(50);
+            sendTextMessageAsync(STEP_8_TEXT, Map.of(
+                    "¡Salir al patio!","step-8"
+            ));
+        }
+
+        if (getCallbackQueryButtonKey().equals("step-8")){
+            sendTextMessageAsync(FINAL_TEXT);
+        }
+
         if(getMessageText().contains("me llamo")){
             sendTextMessageAsync("Encantado de conocerte! Me llamo *Gato*!");
         }
